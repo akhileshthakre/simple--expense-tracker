af194c6d7c28bf2d9ceabf6df61350290936e3ca
Akhilesh Thakre <akhilesh.thakre8@gmail.com>
2021-08-25 00:26:25 +0530

    Successfully established Mongo DB connection

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const dotenv = require('dotenv')
 const colros = require('colors')
 const morgan = require('morgan')
+const connectDB = require('./config/db')
 
 dotenv.config({path: './config/config.env'})
 
+connectDB()
+
 const transactions = require('./routes/transactions')
+
 
 const app = express()
 
